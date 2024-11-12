acfe374a1da6b83cd105e0246b5ac03b2311144b
cyusakeny <gasananike@gmail.com>
2024-11-12 16:56:41 +0200

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,69 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { PiShareNetworkThin } from "react-icons/pi";
+import { HiOutlinePresentationChartLine } from "react-icons/hi2";
+import { LiaFileContractSolid } from "react-icons/lia";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-white w-full h-screen">
+      <nav className=" flex items-center bg-red-500  justify-center h-[10%] rounded-xl ml-4 mr-4">
+        <ul className="flex font-sans flex-row sm:space-x-44 space-x-4 text-lg text-white">
+          <li> <a href="">Home</a></li>
+          <li> <a href="">Contact Us</a></li>
+          <li> <a href="">Join</a></li>
+        </ul>
+      </nav>
+      <div className="mx-[8%] flex sm:flex-row flex-col mt-8">
+          <div className=" sm:w-[50%] w-full sm:pr-6 pr-0 flex flex-col ">
+            <p className="text-4xl text-black font-sans font-bold">Improve Your Productivity</p> 
+            <p className="mt-2 text-3xl text-red-400 font-sans font-bold">Connect With Partners</p>
+            <p className="mt-8 text-justify	">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <button className='w-44 h-16 bg-red-500 text-white text-sans mt-12 rounded-2xl'>
+            Join
+          </button>
+          </div>
+          <div className="sm:w-[50%] w-full  h-[400px] relative">
+          <Image
+            src="/countryside.png"
+            alt="home Image"
+            layout="fill"
+            objectFit="cover"
+            
+      />
+          </div>
+      </div>
+      <div className='sm:mt-32 mt-4 justify-between mx-[8%] flex sm:flex-row flex-col'>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className='sm:w-[20%] w-full mt-4 pb-4 px-2  space-y-4  flex flex-col justify-start'>
+          <p className='w-full text-base font-sans font-bold text-justify'>
+            Connect with strong partners and improve your productivity
+          </p>
+          <div className=' w-full  h-[180px] flex flex-col justify-center items-center rounded-[30px] border-[1px] border-red-500 '>
+            <PiShareNetworkThin className='h-24 w-28 text-sm text-red-500'/>
+            <p className='text-base font-sans font-bold'>Connect With Partners</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className='sm:w-[25%] w-full pb-4 px-2  space-y-4  flex flex-col items-center justify-center'>
+          <p className='w-[80%] text-base font-sans font-bold text-justify'>
+            ShowCase your project and products and attract investors to work with you
+          </p>
+          <div className=' w-full  h-[200px] flex flex-col justify-center items-center rounded-[30px] bg-red-500 border-[1px] border-red-500 '>
+            <HiOutlinePresentationChartLine className='h-24 w-28 text-sm text-white'/>
+            <p className='text-base text-white font-sans font-bold'>ShowCase your products</p>
+          </div>
+        </div>
+        <div className='sm:w-[20%] w-full mt-4 pb-4 px-2  space-y-4  flex flex-col justify-start'>
+          <p className='w-full text-base font-sans font-bold text-justify'>
+            Make Agreements with partners to keep collaborating on  projects
+          </p>
+          <div className=' w-full  h-[180px] flex flex-col justify-center items-center rounded-[30px] border-[1px] border-red-500 '>
+            <LiaFileContractSolid className='h-24 w-28 text-sm text-red-500'/>
+            <p className='text-base font-sans font-bold'>Agreement With Partners</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
